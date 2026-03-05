faa5874dd6f2adfe533db6d2f8158e790f657490
bakerk98 <baker.kevan98@gmail.com>
2026-03-05 11:08:15 -0600

    fixed conditionals

--- move_and_activate_kde_colors.sh
+++ move_and_activate_kde_colors.sh
@@ -3,7 +3,7 @@
 #this script can be run after running pywal16, it'll apply the generated
 #plasma colorscheme using plasma-apply-colorscheme!
 
-if [ -f "~/.cache/wal/modded_breeze.colors" ]; then
+if [ -f ~/.cache/wal/modded_breeze.colors ]; then
     mkdir -p ~/.local/share/color-schemes/
     cp ~/.cache/wal/modded_breeze.colors ~/.local/share/color-schemes/
     #apply breeze dark just in case modded breeze is already selected
@@ -11,7 +11,7 @@ if [ -f "~/.cache/wal/modded_breeze.colors" ]; then
     plasma-apply-colorscheme BreezeDark
     plasma-apply-colorscheme modded_breeze
     echo "Applied pywal16 theme!"
-elif [ -f "~/.cache/wal/kde.colors" ]; then
+elif [ -f ~/.cache/wal/kde.colors ]; then
     mkdir -p ~/.local/share/color-schemes/
     cp ~/.cache/wal/kde.colors ~/.local/share/color-schemes/
     #apply breeze dark just in case kde is already selected
